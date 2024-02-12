fb0b4a7f4dadbd848ac47a8fadfab99ef96780b2
a.churin <a.churin@qiwi.com>
2024-02-13 00:11:52 +0300

    # pre  0.1.1

--- sqldantic/typemap.py
+++ sqldantic/typemap.py
@@ -7,36 +7,36 @@ DEFAULT_TYPE_MAP = {
     ipaddress.IPv6Address: InetType(ipaddress.IPv6Address),
 }
 
-if issubclass(type_, float):
-    return Float
-if issubclass(type_, bool):
-    return Boolean
-if issubclass(type_, int):
-    return Integer
-if issubclass(type_, datetime):
-    return DateTime
-if issubclass(type_, date):
-    return Date
-if issubclass(type_, timedelta):
-    return Interval
-if issubclass(type_, time):
-    return Time
-if issubclass(type_, bytes):
-    return LargeBinary
-if issubclass(type_, Decimal):
-    return Numeric(
-        precision=getattr(metadata, "max_digits", None),
-        scale=getattr(metadata, "decimal_places", None),
-    )
-if issubclass(type_, ipaddress.IPv4Address):
-    return AutoString
-if issubclass(type_, ipaddress.IPv4Network):
-    return AutoString
-if issubclass(type_, ipaddress.IPv6Address):
-    return AutoString
-if issubclass(type_, ipaddress.IPv6Network):
-    return AutoString
-if issubclass(type_, Path):
-    return AutoString
-if issubclass(type_, uuid.UUID):
-    return GUID
+# if issubclass(type_, float):
+#     return Float
+# if issubclass(type_, bool):
+#     return Boolean
+# if issubclass(type_, int):
+#     return Integer
+# if issubclass(type_, datetime):
+#     return DateTime
+# if issubclass(type_, date):
+#     return Date
+# if issubclass(type_, timedelta):
+#     return Interval
+# if issubclass(type_, time):
+#     return Time
+# if issubclass(type_, bytes):
+#     return LargeBinary
+# if issubclass(type_, Decimal):
+#     return Numeric(
+#         precision=getattr(metadata, "max_digits", None),
+#         scale=getattr(metadata, "decimal_places", None),
+#     )
+# if issubclass(type_, ipaddress.IPv4Address):
+#     return AutoString
+# if issubclass(type_, ipaddress.IPv4Network):
+#     return AutoString
+# if issubclass(type_, ipaddress.IPv6Address):
+#     return AutoString
+# if issubclass(type_, ipaddress.IPv6Network):
+#     return AutoString
+# if issubclass(type_, Path):
+#     return AutoString
+# if issubclass(type_, uuid.UUID):
+#     return GUID
